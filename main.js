6796be714fb3f5289446d58836086b0c0bfbf8ba
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2021-12-26 08:02:53 -0600

    conditonals

--- main.js
+++ main.js
@@ -336,6 +336,17 @@ function rentalCarCost(d) {
     return d * 40
   }
 }
+function bmi(weight, height) {
+  if (weight / height ** 2 <= 18.5){
+    return "Underweight"
+  }else if (weight/height ** 2 <= 25.0){
+    return "Normal"
+  }else if (weight/height ** 2 <= 30.0){
+    return "Overweight"
+  }else{
+    return "Obese"
+  }
+}
 //Math Methods
 function litres(time) {
   return Math.floor(time * .5);
