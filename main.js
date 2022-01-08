eb25903216b34b48154dedbdbe41f717631fe7a6
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-01-08 14:38:44 -0600

    switch cases

--- main.js
+++ main.js
@@ -449,6 +449,38 @@ let sheep = ''
     
 }
 //Switch Statements
+function whatday(num) { 
+  // put your code here
+  if (num >= 1 && num <= 7){
+    switch(num) {
+    case 1: 
+      return'Sunday'
+      break;
+     case 2: 
+      return'Monday'
+      break;
+     case 3: 
+      return'Tuesday'
+      break;
+     case 4: 
+      return'Wednesday'
+      break;
+     case 5: 
+      return'Thursday'
+      break;
+    case 6: 
+      return'Friday'
+      break;
+    case 7: 
+      return'Saturday'
+      break;
+}
+  }else{
+    return 'Wrong, please enter a number between 1 and 7'
+  }
+
+
+}
 function getPlanetName(id){
   var name;
   switch(id){
