e5503e004067ea1b7d18ea667d6e4ae1386388fd
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-01-25 05:03:14 -0600

    Modulus and ternaries

--- 8Kyu/main.js
+++ 8Kyu/main.js
@@ -696,6 +696,11 @@ function getPlanetName(id){
   
   return name;
 }
+function remainder(n, m){
+  // Divide the larger argument by the smaller argument and return the remainder
+  return n > m ? n % m : m % n
+  
+}
 
 
 
