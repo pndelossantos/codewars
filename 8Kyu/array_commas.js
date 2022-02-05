e2b18d8d1207ae8325c34f40ff4e6f0648db37b7
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-02-05 11:52:13 -0600

    Refactors for previous solutions

---
+++ 8Kyu/array_commas.js
@@ -0,0 +1,16 @@
+//Instructions
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// Note: if this seems too simple for you try the next level
+
+//Solution
+function printArray(array){
+    //show me the code!
+    return array.join(',')
+  }
---
+++ 8Kyu/repeating_strings.js
@@ -0,0 +1,20 @@
+//Instructions
+
+// Create a function that takes a string and an integer (n).
+
+// The function should return a string that repeats the input string n number of times.
+
+// If anything other than a string is passed in you should return "Not a string"
+
+// Example
+// "Hi", 2 --> "HiHi"
+// 1234, 5 --> "Not a string"
+
+//Solutions
+var repeatIt = function(str, n) {
+    if(typeof str == "string"){
+      return str.repeat(n)
+    }else{
+      return "Not a string"
+    }
+    }
