df280857f697383310dc864b517b3da7461418a2
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-02-06 12:04:03 -0600

    Refactors for previous solutions

---
+++ 8Kyu/doubling_integers.js
@@ -0,0 +1,9 @@
+//Instructions
+//Code as fast as you can! You need to double the integer and return it.
+
+//Solution
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
---
+++ 8Kyu/map_methods.js
@@ -0,0 +1,15 @@
+//Instructions
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//Instructions
+function maps(x){
+    return x.map(num => {
+      return num * 2
+    })
+  
+  }
