403fdb19cfbb829dc93852f0d9cd6dfab7556ffe
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-02-10 13:03:54 -0600

    Refactors for previous solutions

---
+++ 8Kyu/converting_booleans.js
@@ -0,0 +1,16 @@
+//Instructions
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//Solution
+function boolToWord( bool ){
+    //...
+    if (bool === true){
+      return "Yes"
+    }
+    else if (bool === false){
+      return "No"
+    }
+    else{
+      return "Null"
+    }
+  }
