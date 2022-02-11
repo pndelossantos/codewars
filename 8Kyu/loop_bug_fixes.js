85f0c3b4ac2032f65417e1dd696cb098106629a0
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-02-11 06:49:24 -0600

    Adds loop bug fixing kata solution

---
+++ 8Kyu/loop_bug_fixes.js
@@ -0,0 +1,16 @@
+//Instructions
+
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+//Solution
+
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
