599c16643ef30697aed403d4800d001104676b7a
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-03-17 07:49:14 -0500

    Adds potential average kata solution

---
+++ 8Kyu/average_points.js
@@ -0,0 +1,28 @@
+//Instructions
+// An NBA game runs 48 minutes (Four 12 minute quarters). Players do not typically play the full game, subbing in and out as necessary. Your job is to extrapolate a player's points per game if they played the full 48 minutes.
+
+// Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game) and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
+
+// Examples:
+
+// pointsPer48(12, 20) // 28.8
+// pointsPer48(10, 10) // 48 
+// pointsPer48(5, 17) // 14.1 
+// pointsPer48(0, 0) // 0
+// Notes:
+// All inputs will be either be an integer or float.
+// Follow your dreams!
+
+//Solution
+function pointsPer48(ppg, mpg) {
+    //takes in two arguments in integer form representing points per game and minutes played
+    // return how many points a player might have earned had they played the whole game
+    // ((12, 20) 28.8)
+    // catch an 0 inputs, these players would have no points
+      if(ppg === 0){
+        return 0
+      // calculate how many points, round that to one decimal place and ensure the return is an integer
+      }else{
+      return +((48/mpg * ppg).toFixed(1))
+      }
+    }
