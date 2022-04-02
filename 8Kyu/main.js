26bb37dcf65b76c7e576436f4febb137fad3e78b
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-04-02 06:46:27 -0500

    Adds uses of indexOf results

--- 8Kyu/main.js
+++ 8Kyu/main.js
@@ -119,6 +119,24 @@ function calculateTip(amount, rating) {
        return 0
      }
    }
+
+//Checking for wolves
+function warnTheSheep(queue) {
+  let orderedQueue = queue.reverse();
+   for (let i = 0; i < queue.length; i++) {
+     if (orderedQueue[0] === "wolf")
+       return "Pls go away and stop eating my sheep";
+     else
+       return `Oi! Sheep number ${orderedQueue.indexOf(
+         "wolf"
+       )}! You are about to be eaten by a wolf!`;
+   }
+ }
+
+ function warnTheSheep(queue) {
+  const position = queue.reverse().indexOf('wolf');
+  return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+}
   function cockroachSpeed(s) {
     return Math.floor(s * 27.7778)
     //Good Luck!
