1f924974a77aed6608f2ee92c320b58928f6a6fe
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-04-04 23:17:10 -0500

    Rock, Paper, Scissors

--- main.js
+++ main.js
@@ -23,4 +23,14 @@ function squareSum(arr) {
 
 // Sheep Counting
 
+// Rock, Paper, Scissors
+const rps = (p1, p2) => {
+  if(p1 === p2){
+    return "Draw!"
+  }else if ((p1 === "rock" && p2 === "scissors") || (p1 === "scissors" && p2 === "paper") || (p1 === "paper" && p2 === "rock")){
+    return "Player 1 won!"
+  }else{
+    return "Player 2 won!"
+  }
+};
 // Other
