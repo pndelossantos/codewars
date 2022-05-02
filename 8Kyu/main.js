83bdba6ffb06dcc532bf1b1b405082d74e27020e
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-05-02 05:57:49 -0500

    for loop practice

--- 8Kyu/main.js
+++ 8Kyu/main.js
@@ -696,3 +696,15 @@ function getPlanetName(id){
   
   return name;
 }
+
+
+
+
+//For loops
+function sumMul(n,m){
+  if(m <= 0 || n <= 0 ) {return "INVALID";}
+  let res = 0;
+  for(let i = 0; i < m; i+= n){
+  res += i;}
+  return res;
+  }
