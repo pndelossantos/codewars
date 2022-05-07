4fc14c63d5950a24f3783137d0f7da8eb4f15c77
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-05-07 06:34:32 -0500

    Adds test for same case

--- 8Kyu/main.js
+++ 8Kyu/main.js
@@ -109,6 +109,16 @@ function calculateTip(amount, rating) {
     }
     
   }
+  //checking same case
+  function sameCase(a, b){
+    if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+       return -1
+     }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+         return 1
+     }else{
+       return 0
+     }
+   }
   function cockroachSpeed(s) {
     return Math.floor(s * 27.7778)
     //Good Luck!
