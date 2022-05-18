42e083a348ba9074b208413e9e6630449fcef2de
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-05-18 06:56:42 -0500

    Adds notEquals logic problem solution

---
+++ 8Kyu/notA_equalsA.js
@@ -0,0 +1,28 @@
+//Instructions
+// You task to pass only this tests :
+
+// a == false
+// !a == false
+// a == !a
+
+
+//Solution
+
+const a = []
+
+// Second
+// const a = ' '
+
+// Third
+// const a = '0'
+
+// Fourth
+// const a = new Boolean
+
+// Fifth
+// const a = {
+//   num: true,
+//   valueOf: function() {
+//     return !this.num
+//   }
+// }
