a303288722eb37f8c23a0602be01fe2a239da393
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-06-06 08:29:52 -0500

    Adds nth power kata solution

---
+++ 8Kyu/nthEven.js
@@ -0,0 +1,19 @@
+//Instructions
+// Return the Nth Even Number
+
+// Example(Input --> Output)
+
+// 1 --> 0 (the first even number is 0)
+// 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+// 100 --> 198
+// 1298734 --> 2597466
+// The input will not be 0.
+
+//Solution
+function nthEven(n){
+    // take in an integer n
+    // return the nth even number
+    //((2), 2), ((12), 10)
+    
+   return n * 2 -2
+  }
