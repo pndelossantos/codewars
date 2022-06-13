1975278e41abb8fce5cc4f2dafee2f7a92b46446
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-06-13 07:14:10 -0500

    Adds flower petal challenge solution

---
+++ 8Kyu/flowerPetals.js
@@ -0,0 +1,31 @@
+//Instructions
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+//Solution
+function howMuchILoveYou(nbPetals) {
+    // take in an integer of petals
+    // return a phrase associated with how many petals were torn
+    // ((3), 'a lot'
+    // create object with values listed from what the remainder left over would be for each number divided by 6 
+    // (the total number of phrases)
+      let phrase = {    
+        0: "not at all",
+        1: "I love you",
+        2: "a little",
+        3: "a lot",
+        4: "passionately",
+        5: "madly"
+      }
+      // returns the appropriate phrase based on modulos 
+      return phrase[nbPetals%6]
+    }
