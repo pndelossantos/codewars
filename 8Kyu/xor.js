eab7999f225cf38af1c90ae094ace86921a67464
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-06-19 09:13:28 -0500

    Adds xor challenge solution

---
+++ 8Kyu/xor.js
@@ -0,0 +1,20 @@
+//Instructions
+// Exclusive "or" (xor) Logical Operator
+// Overview
+// In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata). The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
+
+// false xor false == false // since both are false
+// true xor false == true // exactly one of the two expressions are true
+// false xor true == true // exactly one of the two expressions are true
+// true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions evaluate to true.
+// Task
+// Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+//Solution
+
+// takes in two boolean values
+// return a boolean value of whether or not only one value is true 
+// ((true, true) false) ((true, false) true)
+function xor(a, b) {
+// checks to see if a and be are two different boolean values aka only one is true
+    return a != b
+    }
