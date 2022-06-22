0be329364ea3112fccd358ba0553195fd48de57b
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-06-22 07:55:49 -0500

    Adds bug fix division challenge solution

---
+++ 8Kyu/bugFix_div.js
@@ -0,0 +1,6 @@
+//Instructions
+// This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+
+//Solution
+const solve = (x, y) => x / y
