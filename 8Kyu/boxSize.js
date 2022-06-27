5eb512a84744b3835324f00f86460c91eb827a51
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-06-27 10:11:52 -0500

    Adds the box size challenge solution

---
+++ 8Kyu/boxSize.js
@@ -0,0 +1,7 @@
+//Instructions
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+//Solution
+function getSize(width, height, depth){
+    return [2 *(width * height + width * depth + height * depth), width * height * depth]
+    }
+    
