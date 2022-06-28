104d212c273be60bf77e425c572a2d2df2edf723
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-06-28 15:24:05 -0500

    Adds the age array difference challenge solution

---
+++ 8Kyu/agesArr.js
@@ -0,0 +1,19 @@
+//Instructions
+// At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+//Solution
+function differenceInAges(ages){
+    let sorted = ages.sort((a,b) => a-b)
+    let young = sorted.shift()
+    let old = sorted.pop()
+    return [young, old, old-young]
+    }
+    
+    //OR
+    function differenceInAges(ages){
+        const min = Math.min(...ages);
+        const max = Math.max(...ages);
+        
+        return [min, max, max-min];
+      }
