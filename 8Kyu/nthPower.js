dd9889e97cfaf0820e53180777c74d57d6bdd50a
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-06-29 11:10:51 -0500

    Adds Nth power challenge solution

---
+++ 8Kyu/nthPower.js
@@ -0,0 +1,19 @@
+//Instructions
+// This kata is from check py.checkio.org
+
+// You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+
+// Let's look at a few examples:
+
+// array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+// array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+//Solution
+function index(array, n){
+    //your code here
+    return array[n] ** n || -1;
+  }
+
+  //takes in an array of integers and an integer n
+  // return the item in index n raised to the nth power
+  // (([1, 2, 3, 4],2)9)
+  // takes the item in the index and raises it, if the item is not in the array return -1
