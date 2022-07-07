1c8553dac79e9881f72e14852cbc06bdf4fce638
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-07-07 08:58:47 -0500

    Adds object and class creation challenge solution

---
+++ 8Kyu/objects.js
@@ -0,0 +1,15 @@
+//Instructions
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+
+//Solution
+var Ball = function(ballType) {
+    this.ballType = ballType || 'regular';
+  };
