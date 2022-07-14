2fd131df2e6cb02ddf3a1cca48b4e17306f49dac
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-07-14 13:36:38 -0500

    Adds shortest word in a string challenge soution

---
+++ 7Kyu/shortestWord.js
@@ -0,0 +1,8 @@
+//Instructions
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+//Solution
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
