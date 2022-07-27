7b8923e29ce6551b4eb47a86dfc9aa1f82a6758e
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-07-27 09:00:36 -0500

    Adds no exclaiming challenge solution

---
+++ 8Kyu/noExclaiming.js
@@ -0,0 +1,24 @@
+//Instructions
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi!!"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+// Note
+// Please don't post issue about difficulty or duplicate.
+//Solution
+function remove (string) {
+    if(string.charAt(string.length-1) == "!"){
+      return string.slice(0, string.length-1)
+    }else{
+      return string
+    }
+  }
+
+  function remove(s) {
+    return s.endsWith('!') ? s.slice(0, -1) : s;
+  }
