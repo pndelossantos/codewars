683063b5849685c91b716647a8ed57058af5e963
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-07-30 21:49:25 -0500

    Adds triple trouble challenge solution

---
+++ 8Kyu/tripleTrouble.js
@@ -0,0 +1,12 @@
+//Instructions
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+// E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+// Note: You can expect all of the inputs to be the same length.
+//Solutions
+function tripleTrouble(one, two, three){
+    //Solution
+    return one.split('').map((e, i) => e + two[i] + three[i]).join('')
+   }
+  
