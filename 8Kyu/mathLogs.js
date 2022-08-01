5004185a999dd6d5f660d0bb955d03e7afe36bfa
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-08-01 11:12:51 -0500

    Adds easy math logs challenge solution

---
+++ 8Kyu/mathLogs.js
@@ -0,0 +1,6 @@
+//Instructions
+// Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+//Solutions
+function logs(x , a, b){
+    return (Math.log(a)/ Math.log(x)) + (Math.log(b)/Math.log(x))
+    }
