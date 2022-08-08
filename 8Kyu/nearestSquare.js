e108ac29885ef2b13d5cb2783b1b6e2cbaffb68e
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-08-08 14:04:37 -0500

    Adds nearest square number challenge solution

---
+++ 8Kyu/nearestSquare.js
@@ -0,0 +1,10 @@
+//Instructions
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// Goodluck :)
+
+
+//Solution
+function nearestSq(n){
+    return Math.round(Math.sqrt(n))*Math.round(Math.sqrt(n))
+    }
