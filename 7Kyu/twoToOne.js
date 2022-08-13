4f2bfc1e64c125364c085518d580323dc30c6b8a
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-08-12 21:26:30 -0500

    Adds two to one challenge solution

---
+++ 7Kyu/twoToOne.js
@@ -0,0 +1,14 @@
+//Instructions
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+//Solution
+function longest(s1, s2) {
+    return (s1+s2).split('').sort().filter((e, i, a)=> e !== a[i-1]).join('');
+  }
