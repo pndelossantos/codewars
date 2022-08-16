15a9b6f046a410dcbc4b44a91e869f89afea1bfb
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-08-16 15:22:37 -0500

    Adds stairs challenge solution

---
+++ 8Kyu/stairs.js
@@ -0,0 +1,31 @@
+//Instructions
+// Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+
+// For example n = 3 result in:
+
+// "I\n I\n  I"
+// or printed:
+
+// I
+//  I
+//   I
+// Another example, a 7-step stairs should be drawn like this:
+
+// I
+//  I
+//   I
+//    I
+//     I
+//      I
+//       I
+
+//Solution
+function drawStairs(n) {
+    let result = [];
+    
+    for (let i = 0; i < n; i++) {
+      result[i] = `${' '.repeat(i)}I`;
+    }
+    
+    return result.join('\n');
+  }
