3a4e9dfdbb638e6c55da386275c94266729e6ab3
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-08-18 21:33:31 -0500

    Adds hexadecimal to decimal challenge solution

---
+++ 8Kyu/hexToDex.js
@@ -0,0 +1,7 @@
+//Instructions
+// Complete the function which converts hex number (given as a string) to a decimal number.
+//Solution
+function hexToDec(hex){
+    //your code here
+    return parseInt(hex, 16)
+  }
