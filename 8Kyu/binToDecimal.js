e35c35bd16b6f8a4fa51c6c6028d3d7ce029e443
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-08-23 13:29:16 -0500

    Adds binary to decimal challenge solution

---
+++ 8Kyu/binToDecimal.js
@@ -0,0 +1,6 @@
+//Instructions
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+//Solution
+function binToDec(bin){
+    return parseInt(bin, 2)
+   }
