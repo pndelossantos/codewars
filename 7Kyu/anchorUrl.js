a8f00012bf6ccd3b1beddf72fd154aa60f1ff136
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-08-26 19:05:38 -0500

    Adds remove anchor url challenge solution

---
+++ 7Kyu/anchorUrl.js
@@ -0,0 +1,25 @@
+//Instructions
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+//Solution
+//P - get a string of varying lengths that may or may not contain #
+//R - return a string with any characters after a # removed
+//E - ('www.codewars.com#about'), 'www.codewars.com')
+//P - 
+function removeUrlAnchor(url){
+    // TODO: complete
+//prep get the index of any #
+    let sliceVal = url.indexOf('#')
+//if the index of slice val is higher than 0 (aka a hashtag is present)
+    if(sliceVal > 0){
+//if so slices the string from beginning to the index of the #
+      return url.slice(0, sliceVal)
+// if not returns the whole string
+    }else{
+      return url
+    }
+  }
