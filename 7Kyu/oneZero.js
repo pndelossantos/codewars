c0da3f08ff01a18dda6cdc34fdc90a62f7520273
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-09-08 11:38:28 -0500

    Adds ones and zeros challenge solution

---
+++ 7Kyu/oneZero.js
@@ -0,0 +1,12 @@
+//Instructions
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+//Solution
+const binaryArrayToNumber = arr => {
+    // your code
+  let binString = arr.join('')
+  return parseInt(binString, 2)
+  };
+
+  const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
