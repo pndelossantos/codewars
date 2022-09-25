9c9239691297b40e8a63c2b4a496ed13a10b6676
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-09-25 18:01:37 -0500

    Adds multiply challenge solution

---
+++ 8Kyu/multiply.js
@@ -0,0 +1,10 @@
+//Instructions
+// This code does not execute properly. Try to figure out why.
+//Solution
+function multiply(a, b){
+//P - parameters takes in two variables of int types
+//R - an integer that is the total of a by b
+//E - (3,4), (12))
+//P - uses the * symbol to multiply a * b and returns using the return statement
+    return a * b
+   }
