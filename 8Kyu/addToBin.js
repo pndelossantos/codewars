048c79a34f0cf4a9f47346c97ef3770abce1c9a1
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-09-30 20:31:43 -0500

    Adds rework of addition to binary challenge solution

--- 8Kyu/addToBin.js
+++ 8Kyu/addToBin.js
@@ -10,7 +10,12 @@
 // NOTE: NaN is a number too in javascript for decimal, binary and n-ary base
 // This is a modification on the Kata: Array2Binary addition hope you like it
 //Solution
+// array of mixed values including numbers and strings
+// return the binary equivalent of the sum of all integers/number elements in the array
+//
+// .filter method is used to filter out all array elements of the type of number, those values are then reduced into a single sum using the .reduce method (with a 0 to ensure the result is a number) that sum is then converted into binary using the toString(2) method
 function arr2bin(arr){
+
     // show me the code
      return arr.filter(e => typeof(e) === 'number').reduce((acc, cv) => acc + cv, 0).toString(2)
   }
