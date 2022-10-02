a550eb62a552d449764bdce3987c669cdfea0ee6
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-01 20:55:45 -0500

    Adds rework of animal years challenge solution

--- 8Kyu/agesArr.js
+++ 8Kyu/agesArr.js
@@ -3,6 +3,8 @@
 
 // You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
 //Solution
+
+
 function differenceInAges(ages){
     let sorted = ages.sort((a,b) => a-b)
     let young = sorted.shift()
@@ -11,6 +13,10 @@ function differenceInAges(ages){
     }
     
     //OR
+    // get an array of familial ages, only integers in the whole value numbers
+    // return an array with the youngest age, oldest age, and difference in that order
+    // Using the math.min and math.max methods and the spread operator to store the smallest and largest operators
+    // finally those values and the difference will be returned 
     function differenceInAges(ages){
         const min = Math.min(...ages);
         const max = Math.max(...ages);
--- 8Kyu/animalYears.js
+++ 8Kyu/animalYears.js
@@ -20,6 +20,12 @@
 // +5 dog years for each year after that
 
 //Solution
+//P - receiving a number stored in the humanYears
+//R - return an array of numbers that represent the amount of human years, cat years, and dog years
+//E - 
+//P - create several variables to store values to be placed in the final array. for catYears if the human years are less than or equal to 1
+// the value returned is human years times 15 if not the value of 16 + human * 4 is stored in catYears
+// a similar process follows for the dog years variable, the values are then plugged into the returned array
 function humanYearsCatYearsDogYears(humanYears) {
     let catYears = (humanYears <= 1)? humanYears * 15 : 16 + humanYears * 4;
     let dogYears = (humanYears <= 1)? humanYears * 15 : 14 + humanYears * 5;
