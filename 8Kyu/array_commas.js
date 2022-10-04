f435d8b0a4619849f96c8ff81b7b388dc6cd81be
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-03 21:40:42 -0500

    Adds arrays joined by commas challenge solution

--- 8Kyu/array_commas.js
+++ 8Kyu/array_commas.js
@@ -10,6 +10,10 @@
 // Note: if this seems too simple for you try the next level
 
 //Solution
+// an array of elements
+// string with elements separated by commas
+// ((["l", "o", "l"]) lol)
+// uses the .join method to join elements and specifies the elements should be joined with a comma
 function printArray(array){
     //show me the code!
     return array.join(',')
