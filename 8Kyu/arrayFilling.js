dda581d97d4cd3a315e61eb9f0cbd6147435561d
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-04 21:48:20 -0500

    Adds array filling challenge solution

--- 8Kyu/arrayFilling.js
+++ 8Kyu/arrayFilling.js
@@ -8,10 +8,16 @@
 // arr(5) // => [0,1,2,3,4]
 // Note: The parameter is optional. So you have to give it a default value.
 //Solution
+// a single integer/number
+// return an array from 0 till the given value of n
+// ((3) 0 1 2 3)
+// created a newArr variable that is an empty array
+// using a for loop to start at 0 and with a stopping condition of less than n and increments by +1 , loop through
+// and then push that value to the newArr variable then newArr is returned
 function arr(n){
     var newArr = [];
     for(var i = 0; i < n; i++){
       newArr.push(i);
     }
     return newArr;
-  }
+  
