569cc135c54355d5e79f020cd211dcdaee143ad9
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-05 21:54:23 -0500

    Adds array filtering with arrow syntax challenge solution

--- 8Kyu/arrowFunc.js
+++ 8Kyu/arrowFunc.js
@@ -4,6 +4,10 @@
 // [1, 2, 3, 4, 5]  -->  [1, 3, 5]
 // [2, 4, 6]        -->  []
 //Solution
+// takes an array of numbers/integers
+// returns an array of all odd values 
+// see instructions
+// uses the .filter method to check if each element is even or odd using modulus to compare the results to 1 if so the values are added to the newly returned array
 function odds(values){
     // arrow it
     return values.filter(e => e%2 == 1 );
