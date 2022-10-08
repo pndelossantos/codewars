c241814d048784232692b451d77f723ece6efdd5
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-07 22:15:45 -0500

    Adds reworking of convert to decimal challenge solution

--- 8Kyu/binToDecimal.js
+++ 8Kyu/binToDecimal.js
@@ -1,6 +1,10 @@
 //Instructions
 // Complete the function which converts a binary number (given as a string) to a decimal number.
 //Solution
+// a binary integer
+// the decimal equivalent of the binary number
+// 
+// uses the parseInt function to take the binary variable and 2 and the point of conversion to convert the number
 function binToDec(bin){
     return parseInt(bin, 2)
    }
