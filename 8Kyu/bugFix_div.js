4b884cc489b943e766b1c8f8a6904615f2b9fa39
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-12 14:08:49 -0500

    Adds rework bug fix division challenge solution

--- 8Kyu/bugFix_div.js
+++ 8Kyu/bugFix_div.js
@@ -3,4 +3,8 @@
 
 
 //Solution
+// to variables x and y each of which contains a number
+// return the result of x divided by y in the form of an integer
+// ((4,2) 2)
+// Uses the / operator and the arrow function syntax to return the result of x divided by y
 const solve = (x, y) => x / y
