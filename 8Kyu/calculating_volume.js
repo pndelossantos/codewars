1d557bc231a5b36f304640426ed504e0d77dad2f
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-13 14:13:40 -0500

    Adds calculating volume challenge solution

--- 8Kyu/calculating_volume.js
+++ 8Kyu/calculating_volume.js
@@ -3,9 +3,13 @@
 // Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
 
 //Solutions
+// 3 variables, all integers
+// return the result of multiplying the the three variables in an integer format
+// ((2, 3. 3) 18)
+// uses the * operator to multiple variables against each other
 class Kata {
     static getVolumeOfCuboid(length, width, height) {
-      // Calcutlate Volume
+      // Calculate Volume
       return length * width * height
     }
   }
