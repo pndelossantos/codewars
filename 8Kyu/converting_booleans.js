9dd2929640ad5d1aef057a4ccc7e33c5ca7087f0
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-16 14:36:43 -0500

    Adds converting booleans challenge rework solution

--- 8Kyu/converting_booleans.js
+++ 8Kyu/converting_booleans.js
@@ -2,6 +2,10 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 //Solution
+// boolean value of true false 
+// return string depending on the boolean value, "Yes" for true, "No" for false, "Null" for all other inputs
+// ((true) "Yes")
+// uses an if/else statement and checks the value of bool to determine what string to return
 function boolToWord( bool ){
     //...
     if (bool === true){
