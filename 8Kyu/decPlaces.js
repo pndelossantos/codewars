5b49f673a896670cb1cfeee299cd1eae59222d36
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-18 14:48:40 -0500

    Adds two decimal places challenge rework solution

--- 8Kyu/decPlaces.js
+++ 8Kyu/decPlaces.js
@@ -6,6 +6,10 @@
 // 3.3424 is rounded 3.34
 
 //Solution
+// takes in an element n of the integer/float type
+// return an integer rounded to 2 decimal places
+// see above
+// uses toFixed(2) to round the n to two decimal places and uses the + operator to ensure the output is a number
 function twoDecimalPlaces(n) {
     // Your code here
     return +n.toFixed(2)
