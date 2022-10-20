0f2a9116d0fe8c11bf931761b9c82ed4f4c827cf
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-20 14:56:08 -0500

    Adds doubling integers challenge rework solution

--- 8Kyu/doubling_integers.js
+++ 8Kyu/doubling_integers.js
@@ -2,7 +2,10 @@
 //Code as fast as you can! You need to double the integer and return it.
 
 //Solution
-
+// i an integer
+// return i doubled in integer format
+// ((3) 6)
+// uses the * operator to double i and return the appropriate integer
 function doubleInteger(i) {
     // i will be an integer. Double it and return it.
     return i * 2;
