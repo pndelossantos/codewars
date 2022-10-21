543b8d7196be2b5012ea20077ec13ccdd5b17739
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-21 15:11:55 -0500

    Adds how many sheep challenge rework solution

--- 8Kyu/filter_methods.js
+++ 8Kyu/filter_methods.js
@@ -14,6 +14,10 @@
 // Hint: Don't forget to check for bad values like null/undefined
 
 //Solution
+// takes in an array of boolean values with true representing sheep and false representing no sheep 
+// the amount of sheep aka true booleans
+// see above
+// uses the filter method on the sheep array and creates a new array of all the true boolean values, the length of this new array is returned as the representation of the amount of sheep
 
 function countSheeps(arrayOfSheep) {
     // TODO May the force be with you
