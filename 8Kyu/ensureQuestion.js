a122684139ea099b71e46b52b10740f7d6a32ac6
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-22 15:08:23 -0500

    Adds is it a question challenge rework solution

--- 8Kyu/ensureQuestion.js
+++ 8Kyu/ensureQuestion.js
@@ -7,6 +7,11 @@
 // "No?" --> "No?"
 
 //Solution
+// an input string stored in the s variable
+// return the same string in the form of a question if it is not already in the form of a questions
+// see above
+// splits the original input into individual characters and then checks to see if ? is included in those characters, if so the original string is returned as it is already a question.
+// if not the original string and a concat "?" are returned
 function ensureQuestion(s) {
     // Code here
     return s.split('').includes("?") ? s : s + "?"
