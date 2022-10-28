6b75b384b7bbc50ed0014624f3cbeb0992428bb5
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-28 18:15:45 -0500

    Adds pre FizzBuzz challenge solution

---
+++ 8Kyu/preFizzBuz.js
@@ -0,0 +1,16 @@
+//Instructions
+// This is the first step to understanding FizzBuzz.
+
+// Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+
+// Your expected output is an array of positive integers from 1 to n (inclusive).
+
+// Your job is to write an algorithm that gets you from the input to the output.
+//Solution
+function preFizz(n) {
+    let array = []
+    for(i = 1; i <= n; i++){
+      array.push(i)
+    }
+      return array
+    }
