e6acab682a76c5ea24b2075a53aced9d11834e91
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-30 17:32:05 -0500

    Adds includes english challenge solution

---
+++ 8Kyu/speakEnglish.js
@@ -0,0 +1,14 @@
+//InstructionsGiven a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+
+
+//Solution
+function spEng(sentence){
+    //write your code here
+      return sentence.toLowerCase().includes('english')
+    }
