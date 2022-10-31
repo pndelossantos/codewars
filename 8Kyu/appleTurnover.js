a8ecbc6fdda70678f1cf34a2ba166958441a91c5
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-10-31 15:38:07 -0500

    Adds apple turnover challenge solution

---
+++ 8Kyu/appleTurnover.js
@@ -0,0 +1,8 @@
+//Instructions
+//Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// Note: Input will either be a positive integer (or a string for untyped languages).
+//Solution
+function apple(x){
+    return Number(x**2) > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+  }
