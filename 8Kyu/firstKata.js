e058d1d90185a82846157ea809e3e0bcd3027f0c
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-07 15:35:03 -0600

    Adds first kata     challenge rework solution

--- 8Kyu/firstKata.js
+++ 8Kyu/firstKata.js
@@ -6,7 +6,10 @@
 // You may assume the following:
 
 // If a and b are both numbers, neither of a or b will be 0.
-//Solution
+// - return false if one of the two inputs is not a number, if both inputs are numbers return the result of a and b divided by one another and added together 
+// - ((3, b) false)
+// - using an if else statement the if checks if the typeof each input variable is not a number, if either is not a number false is returned
+// if both inputs are numbers the else will run the remainders of a %b and b%a are added together and returned
 function myFirstKata(a, b){
     if(typeof(a) != "number" || typeof(b) != "number"){
       return false
