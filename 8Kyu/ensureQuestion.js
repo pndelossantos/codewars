2f020f8b37f68833971ee47de87fcd20c5beff22
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-08 13:52:54 -0600

    Adds ensure question challenge solution

---
+++ 8Kyu/ensureQuestion.js
@@ -0,0 +1,13 @@
+//Instructions
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+// For example (Input --> Output)
+
+// "Yes" --> "Yes?" 
+// "No?" --> "No?"
+
+//Solution
+function ensureQuestion(s) {
+    // Code here
+    return s.split('').includes("?") ? s : s + "?"
+  }
