59636301b476f54a9ccb721b3482b544165139ab
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-09 15:40:45 -0600

    Adds health calculations challenge rework solution

--- 8Kyu/healthCalculations.js
+++ 8Kyu/healthCalculations.js
@@ -1,6 +1,11 @@
 //Instructions
 // Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
 //Solution
+// takes in two inputs of integer types
+// returns how much health is left after the damage is received 
+// ((20, 5) 15)
+// using an if else statement the if checks to see if the damage integer is greater than the health integer. If so 0 is returned as health can not drop below 0.
+// if damage is not greater than health the difference is returned
 function combat(health, damage) {
     // Write your code here
     if(damage > health){
