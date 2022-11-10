8d39a99551ece9111403af3add20f32340acc035
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-10 16:06:44 -0600

    Adds hello challenge solution

--- 8Kyu/hello.js
+++ 8Kyu/hello.js
@@ -11,6 +11,11 @@
 // * With `name` not given 
 //   or `name` = ""        => return "Hello, World!"
 //Solution
+
+//takes in a string "name" which can be of varying lengths and various cases or nothing
+// return a string Hello, Name(with proper upper case/lower case letters.)
+// see above 
+//returns a template literal string with Hello, and if name is present the first letter of name changed to upper case using toUpperCase and the rest of the name sliced and changed to lower case. If no name is given returns Hello, world. 
 function hello(name) {
     return `Hello, ${name ? name[0].toUpperCase() + name.slice(1).toLowerCase() : "World"}!`
   }
