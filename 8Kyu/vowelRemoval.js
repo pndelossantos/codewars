4a0764fffadc881f3b4d81149229da11f528598d
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-11 16:13:32 -0600

    Adds vowel removal challenge solution

--- 8Kyu/vowelRemoval.js
+++ 8Kyu/vowelRemoval.js
@@ -9,6 +9,11 @@
 // don't worry about uppercase vowels
 // y is not considered a vowel for this kata
 //Solution
+// takes in a string of varying lengths/cases
+// return the input string with all lower case vowels removed, y is not considered a vow for this challenge
+// see above 
+// the original string is split into individual letters and then the filter method is used on the newly split string array and any non lower case vowels are passed on to the new array and joined together 
+// using .join to return the original string - lower case vowels
 function shortcut (string) {
     return string
       .split('').filter(str => !'aeiou'.includes(str)).join('')
