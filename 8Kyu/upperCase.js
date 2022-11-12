b41f48ff0b8e36192994801e0803bd0ba640d9b7
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-12 16:22:44 -0600

    Adds to upper case challenge solution

--- 8Kyu/upperCase.js
+++ 8Kyu/upperCase.js
@@ -2,6 +2,10 @@
 // Write a function which converts the input string to uppercase.
 
 //Solution
+// takes in a string with no specific attributes
+// return the input string changed to upper case
+// ((dog) DOG)
+// returns the input string after applying the .toUpperCase() method which will change the case of all letters
 function makeUpperCase(str) {
     // Code here
     return str.toUpperCase()
