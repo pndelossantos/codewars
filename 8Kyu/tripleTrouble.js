7fc48f01c9bd8574a49510e598aac3c8702e128a
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-13 16:41:30 -0600

    Adds triple trouble challenge solution

--- 8Kyu/tripleTrouble.js
+++ 8Kyu/tripleTrouble.js
@@ -5,6 +5,10 @@
 
 // Note: You can expect all of the inputs to be the same length.
 //Solutions
+// takes in three input strings
+// return a string that takes all the 1st, 2nd, 3rd letters of the input strings and places them next to each other
+// see above
+// takes the first input and splits it into individual letters then maps each element of the newly split array and the elements of the same index in the other 2 inputs together and then rejoins them
 function tripleTrouble(one, two, three){
     //Solution
     return one.split('').map((e, i) => e + two[i] + three[i]).join('')
