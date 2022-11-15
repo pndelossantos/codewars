3db7aa13d380589a9e4d4e1036fbe62456d9502e
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-14 18:09:49 -0600

    Adds summation challenge solution

--- 8Kyu/summation_variable.js
+++ 8Kyu/summation_variable.js
@@ -12,6 +12,11 @@
 // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 
 //Solution
+//takes in an integer number
+// return the sum from 1 to the given number
+//see above
+// multiplies the number by the number + 1 divided by 2
+
 var summation = function (num) {
     // Code here
     return num*(num + 1)/2
