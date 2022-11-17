78afe86bc1f689d56efd25e40b1f69d14053d144
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-16 18:15:57 -0600

    Adds square function challenge solution rework

--- 8Kyu/square_function.js
+++ 8Kyu/square_function.js
@@ -2,6 +2,11 @@
 // Now you have to write a function that takes an argument and returns the square of it.
 
 //Solution
+// takes in an integer number num
+// return the num input squared
+// ((2) 4)
+// multiplies the num by itself to square it and the result is returned, the pow function could also abe used
+
 function square(num){
     return num * num
   }
