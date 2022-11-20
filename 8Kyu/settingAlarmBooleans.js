e3e372a019bf0de7c42c7bef8077e84d0de98302
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-19 18:34:33 -0600

    Adds alarm boolean challenge solution rework

--- 8Kyu/settingAlarmBooleans.js
+++ 8Kyu/settingAlarmBooleans.js
@@ -10,6 +10,10 @@
 
 
 //Solution\
+// takes in two boolean inputs stored within the employed and vacation parameters
+// return a boolean value reflecting whether or not an alarm needs to be set (true) or not (false), if employed is true and vacation is false true must be returned
+// see line above
+// using the strict equality operator and the logical or operator returns whether employed is true and vacation is false from the provided parameters 
 function setAlarm(employed, vacation){
   
     return employed === true && vacation === false 
