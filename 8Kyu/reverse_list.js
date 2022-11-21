fd794dc09d65e02523e64ac1124bcd80fa5a98b6
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-20 18:46:17 -0600

    Adds reverse list challenge solution rework

--- 8Kyu/reverse_list.js
+++ 8Kyu/reverse_list.js
@@ -6,6 +6,10 @@
 // * [9, 2, 0, 7]  -> [7, 0, 2, 9]
 
 //Solution
+// takes in an array with varying elements
+// return the same array but with the order of elements reversed
+// see above
+// the input array is reversed by using the built in reverse method on the list input, the result is returned
 function reverseList(list) {
     return list.reverse()
    }
