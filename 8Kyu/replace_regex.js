95bad4c664070be4f9cb3eb508f33500f5ae6388
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-11-21 18:49:17 -0600

    Adds replace regex challenge solution rework

--- 8Kyu/replace_regex.js
+++ 8Kyu/replace_regex.js
@@ -12,7 +12,10 @@
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
 //Solution
-
+// takes in a string input stored in the dna parameter
+// return the original string with any instance of "T" replaced with "U"
+// see above 
+// the replace method and regex is used to replace any T (using the global g) with "U", the result is returned
 function DNAtoRNA(dna) {
     // create a function which returns an RNA sequence from the given DNA sequence
     return dna.replace(/T/g, "U")
