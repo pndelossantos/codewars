cfbcc6074947992c9dcc40848f01577ec1f384ff
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-12-01 20:18:33 -0600

    Adds reduc method challenge solution rework

--- 8Kyu/reduce_methods.js
+++ 8Kyu/reduce_methods.js
@@ -10,7 +10,10 @@
 // Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 
 //Solution
-
+// takes in an array filled with integer numbers in the form of parameter classPoints and an integer number in yourPoints
+// return a boolean of whether or not your points are greater than the average of classPoints
+// 
+// uses the reduce method on the classPoints array to sum up all values and then that values will be divided by the length of the array to get the average. That average will then be compared to your points using the < operator which will return the appropriate boolean value
 function betterThanAverage(classPoints, yourPoints) {
     // Your code he
     return classPoints.reduce((accumulator, currentValue) => {
