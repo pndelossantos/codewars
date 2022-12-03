5fd91097be7a697b8a46570e1ff8d878f6f444d3
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-12-02 20:21:41 -0600

    Adds reduce math method challenge solution rework

--- 8Kyu/reduce_method_maths.js
+++ 8Kyu/reduce_method_maths.js
@@ -4,6 +4,10 @@
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
 //Solution
+// takes in a non-empty array of integers
+// return the result of all elements being multiplied in order
+// see above
+// the reduce function is used and the acc and cv are multiplied until all elements in the array are used. the result is then returned
 function grow(x){
     return x.reduce((accumulator, currentValue) => {  
       return accumulator * currentValue;
