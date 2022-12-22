20640bc33b53f9ed786c87f8d4efcf571cf47f2f
Priscilla De Los Santos <96341684+pndelossantos@users.noreply.github.com>
2022-12-22 10:16:26 -0600

    Adds six toast challenge solution

---
+++ 8Kyu/six_toast.js
@@ -0,0 +1,15 @@
+//Instructions
+// Define a function that counts how many more (or less) pieces of toast you need in the toasters. Even though you need more or less, the number will still be positive, not negative.
+
+// Examples:
+// You must return the number of toast the you need to put in (or to take out). In case of 5 you can still put 1 toast in:
+
+// six_toast(5) == 1
+// And in case of 12 you need 6 toasts less (but not -6):
+
+// six_toast(12) == 6
+
+//Solution
+function sixToast(num) {
+    return Math.abs(6 - num)
+    }
